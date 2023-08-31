599101654e2aaa5a497ed05738a720d7827a0dd6
David Morrison <drmorr@appliedcomputing.io>
2023-08-31 15:39:42 -0700

    strip unnecessary stuff from trace pod spec

--- lib/rust/watchertracer/watcher.rs
+++ lib/rust/watchertracer/watcher.rs
@@ -14,6 +14,7 @@ use kube::runtime::watcher::{
     Error,
     Event,
 };
+use kube::runtime::WatchStreamExt;
 use tracing::*;
 
 use crate::util::{
@@ -30,11 +31,24 @@ pub struct Watcher {
     clock: Arc<Mutex<dyn Clockable>>,
 }
 
+fn strip_pod(pod: &mut corev1::Pod) {
+    pod.metadata.uid = None;
+    pod.metadata.resource_version = None;
+    pod.metadata.managed_fields = None;
+    pod.metadata.creation_timestamp = None;
+    pod.metadata.deletion_timestamp = None;
+    if let Some(ref mut pspec) = pod.spec {
+        pspec.service_account = None;
+        pspec.service_account_name = None;
+    }
+}
+
 impl Watcher {
     pub fn new(client: kube::Client, t: Arc<Mutex<Tracer>>) -> Watcher {
         let pods: kube::Api<corev1::Pod> = kube::Api::all(client);
+        let pod_stream = watcher(pods, Default::default()).modify(strip_pod);
         return Watcher {
-            w: watcher(pods, Default::default()).boxed(),
+            w: pod_stream.boxed(),
             clock: Arc::new(Mutex::new(UtcClock {})),
             t,
         };
@@ -47,13 +61,12 @@ impl Watcher {
     pub async fn start(&mut self) {
         loop {
             tokio::select! {
-                item = self.w.next() => match item {
-                    None => break,
-                    Some(res) => match res {
+                item = self.w.next() => if let Some(res) = item {
+                    match res {
                         Ok(evt) => self.handle_pod_event(evt),
                         Err(e) => error!("tracer received error on stream: {}", e),
-                    },
-                },
+                    }
+                } else { break },
             }
         }
     }
