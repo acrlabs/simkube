92d6495d6a9c44d9a221da0b3937ecdce3848be8
David Morrison <drmorr@appliedcomputing.io>
2024-06-28 10:43:55 -0700

    add completions to cli

--- cli/main.rs
+++ cli/main.rs
@@ -1,3 +1,4 @@
+mod completions;
 mod crd;
 mod delete;
 mod export;
@@ -5,6 +6,7 @@ mod run;
 mod snapshot;
 
 use clap::{
+    CommandFactory,
     Parser,
     Subcommand,
 };
@@ -16,14 +18,17 @@ use simkube::prelude::*;
     version,
     propagate_version = true
 )]
-struct Options {
+struct SkCommandRoot {
     #[command(subcommand)]
-    subcommand: Commands,
+    subcommand: SkSubcommand,
 }
 
 #[derive(Subcommand)]
 #[allow(clippy::large_enum_variant)]
-enum Commands {
+enum SkSubcommand {
+    #[command(about = "generate shell completions for skctl")]
+    Completions(completions::Args),
+
     #[command(about = "print SimKube CRDs")]
     Crd,
 
@@ -42,13 +47,17 @@ enum Commands {
 
 #[tokio::main]
 async fn main() -> EmptyResult {
-    let args = Options::parse();
+    let args = SkCommandRoot::parse();
 
     match &args.subcommand {
-        Commands::Crd => crd::cmd(),
-        Commands::Export(args) => export::cmd(args).await,
-        Commands::Delete(args) => delete::cmd(args).await,
-        Commands::Run(args) => run::cmd(args).await,
-        Commands::Snapshot(args) => snapshot::cmd(args).await,
+        SkSubcommand::Completions(args) => completions::cmd(args, SkCommandRoot::command()),
+        SkSubcommand::Crd => crd::cmd(),
+        SkSubcommand::Export(args) => export::cmd(args).await,
+        SkSubcommand::Delete(args) => delete::cmd(args).await,
+        SkSubcommand::Run(args) => run::cmd(args).await,
+        SkSubcommand::Snapshot(args) => snapshot::cmd(args).await,
     }
 }
+
+#[cfg(test)]
+mod tests;
