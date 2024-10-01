dbdbc4591f994c07c62e7d58442cf7a6dd12abe8
David Morrison <drmorr@appliedcomputing.io>
2024-10-01 14:20:18 -0700

    adding snapshot tests for xray

---
+++ sk-cli/src/xray/tests/testutils.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+#[macro_export]
+macro_rules! set_snapshot_suffix {
+    ($($expr:expr),*) => {
+        let mut settings = insta::Settings::clone_current();
+        settings.set_snapshot_suffix(format!($($expr,)*));
+        let _guard = settings.bind_to_scope();
+    }
+}
