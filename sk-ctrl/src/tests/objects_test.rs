f5e72cb436cc66adb5495ec66e6c9da446fe3382
David Morrison <drmorr@appliedcomputing.io>
2025-05-14 11:44:36 -0700

    feat: skip local volume mount flag

--- sk-ctrl/src/tests/objects_test.rs
+++ sk-ctrl/src/tests/objects_test.rs
@@ -1,9 +1,14 @@
 use std::env;
 
 use assertables::*;
+use tracing_test::traced_test;
 
 use super::*;
-use crate::objects::build_driver_job;
+use crate::objects::{
+    build_driver_job,
+    build_local_trace_volume,
+    TRACE_VOLUME_NAME,
+};
 
 #[rstest(tokio::test)]
 async fn test_build_driver_job_with_extra_args(mut test_sim: Simulation) {
@@ -19,7 +24,7 @@ async fn test_build_driver_job_with_extra_args(mut test_sim: Simulation) {
 
     let job_spec = job.spec.unwrap().template.spec.unwrap();
     let args = job_spec.containers.get(0).unwrap().args.as_ref().unwrap();
-    let expected_args: Vec<&str> = vec![
+    let expected: Vec<&str> = vec![
         "--cert-path",
         "/usr/local/etc/ssl/tls.crt",
         "--key-path",
@@ -36,5 +41,47 @@ async fn test_build_driver_job_with_extra_args(mut test_sim: Simulation) {
         "bar",
         "--baz",
     ];
-    assert_iter_eq!(expected_args, args);
+    assert_iter_eq!(args, expected);
+}
+
+#[rstest]
+fn test_build_local_trace_volume_not_local(mut test_sim: Simulation) {
+    test_sim.spec.driver.trace_path = "s3://foo/bar".into();
+    let res = build_local_trace_volume(&test_sim).unwrap();
+    assert_none!(res);
+}
+
+#[rstest]
+fn test_build_local_trace_volume_skip_volume_mount(mut test_sim: Simulation) {
+    test_sim
+        .annotations_mut()
+        .insert(SKIP_LOCAL_VOLUME_MOUNT_ANNOTATION_KEY.into(), "true".into());
+    let res = build_local_trace_volume(&test_sim).unwrap();
+    assert_none!(res);
+}
+
+#[rstest]
+#[traced_test]
+fn test_build_local_trace_volume_skip_volume_mount_not_local(mut test_sim: Simulation) {
+    test_sim
+        .annotations_mut()
+        .insert(SKIP_LOCAL_VOLUME_MOUNT_ANNOTATION_KEY.into(), "true".into());
+    test_sim.spec.driver.trace_path = "s3://foo/bar".into();
+    let res = build_local_trace_volume(&test_sim).unwrap();
+    assert_none!(res);
+    assert!(logs_contain("ignoring annotation"));
+}
+
+#[rstest]
+fn test_build_local_trace_volume(test_sim: Simulation) {
+    let res = build_local_trace_volume(&test_sim).unwrap();
+    let (mount, volume, path) = res.unwrap();
+
+    assert_eq!(mount.name, TRACE_VOLUME_NAME);
+    assert_eq!(mount.mount_path, "/foo/bar");
+    assert_eq!(volume.name, TRACE_VOLUME_NAME);
+    let host_path = volume.host_path.unwrap();
+    assert_eq!(host_path.path, "/foo/bar");
+    assert_eq!(host_path.type_, Some("File".into()));
+    assert_eq!(path, "/foo/bar");
 }
