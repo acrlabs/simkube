2e3f04a58db44e44ec3b3e8dafdf0e8daed7f812
Ian O'Gorman <ogorman89@gmail.com>
2026-05-26 10:12:14 -0700

    remove comment

--- sk-cli/src/snapshot.rs
+++ sk-cli/src/snapshot.rs
@@ -42,7 +42,6 @@ pub async fn cmd(args: &Args) -> EmptyResult {
     let filters = ExportFilters::new(args.excluded_namespaces.clone(), vec![]);
     let start_ts = UtcClock.now_ts();
     let end_ts = start_ts + 1;
-    // Snapshot should probably also accept an optional transformation file for consistency
     let data = manager
         .get_store()
         .lock()
