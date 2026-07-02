d3d07f47441b6cd088c3f2fd270e19def46e9341
Ian O'Gorman <ogorman89@gmail.com>
2026-07-02 11:41:39 -0700

    filter in collect events, clean up test files

--- sk-tracer/src/store.rs
+++ sk-tracer/src/store.rs
@@ -1,8 +1,5 @@
 use std::collections::HashMap;
-use std::sync::{
-    Arc,
-    mpsc,
-};
+use std::sync::Arc;
 
 use kube::Resource;
 use sk_api::v1::ExportFilters;
@@ -73,6 +70,7 @@ impl TraceStore {
         // true so that in the second step, we keep pod data around even if the owning object was
         // deleted before the trace ends.
         let (events, index) = self.collect_events(start_ts, end_ts, filter, true, maybe_skel_file).await?;
+        // let num_events = events.len();
 
         // Collect all pod lifecycle data that is a) between the start and end times, and b) is
         // owned by some object contained in the trace
@@ -83,7 +81,8 @@ impl TraceStore {
             index,
             pod_lifecycles,
             ..Default::default()
-        };
+        }
+        .to_bytes()?;
 
         Ok(data)
     }
